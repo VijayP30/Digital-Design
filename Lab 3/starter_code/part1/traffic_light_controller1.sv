28ee83ed0a1da4343c94f4c66ef717ed62db791c
pvijay03 <pvijay@ucsd.edu>
2024-05-25 15:39:01 -0700

    working part1 code

--- Lab 3/starter_code/part1/traffic_light_controller1.sv
+++ Lab 3/starter_code/part1/traffic_light_controller1.sv
@@ -7,10 +7,10 @@
 import light_package ::*;           // defines red, yellow, green
 
 // same as Harris & Harris 4-state, but we have added two all-reds
-module traffic_light_controller1(
+module traffic_light_controller(
   input  clk, reset, 
          s_s, l_s, n_s,  // traffic sensors, east-west straight, east-west left, north-south 
-  output colors ew_str_light, ew_left_light, ns_light);    // traffic lights, east-west straight, east-west left, north-south
+  output colors str_light, left_light, ns_light);    // traffic lights, east-west straight, east-west left, north-south
 
 // HRR = red-red following YRR; RRH = red-red following RRY;
 // ZRR = 2nd cycle yellow, follows YRR, etc. 
@@ -25,14 +25,14 @@ module traffic_light_controller1(
 // combinational part will reset or increment the counters and figure out the next_state
   always_ff @(posedge clk)
     if(reset) begin
-	  present_state <= RRH;
+	    present_state <= RRH;
       ctr5          <= 0;
       ctr10         <= 0;
     end  
-	else begin
-	  present_state <= next_state;
-      ctr5          <= next_ctr5;
-      ctr10         <= next_ctr10;
+	  else begin
+	    present_state  <= next_state;
+      ctr5           <= next_ctr5;
+      ctr10          <= next_ctr10;
     end  
 
 // combinational part of state machine ("C1" block in the Harris & Harris Moore machine diagram)
@@ -43,26 +43,123 @@ module traffic_light_controller1(
     next_ctr10 = 0;
     case(present_state)
 /* ************* Fill in the case statements ************** */
-	  GRR: begin 
-         // when is next_state GRR? YRR?
-         // what does ctr5 do? ctr10?
-      end  
-     // etc. 
+	  GRR: begin
+        // Check for 5 cycles of no traffic, or 10 cycles of traffic
+        if (ctr5 == 4 || ctr10 == 9) begin
+          next_state = YRR;
+          next_ctr5 = 0;
+          next_ctr10 = 0;
+        end
+        else begin
+          // Stay in the same state, increment counters
+          next_state = GRR;
+          if (ctr5 != 0)
+            next_ctr5 = ctr5 + 1;
+          else if (!s_s)
+            next_ctr5 = 1;
+          if (ctr10 != 0)
+            next_ctr10 = ctr10 + 1;
+          else if (s_s && (l_s || n_s))
+            next_ctr10 = 1;
+        end
+      end
+    // Brute force way to make yellow last 2 cycles
+    YRR: next_state = ZRR;
+    ZRR: next_state = HRR;
+    // Check the sensors to determine the next state via priority
+    HRR: begin
+      if (l_s)
+        next_state = RGR;
+      else if (n_s)
+        next_state = RRG;
+      else if (s_s)
+        next_state = GRR; 
+      else
+        next_state = HRR;
+      end
+    RGR: begin
+        // Check for 5 cycles of no traffic, or 10 cycles of traffic
+        if (ctr5 == 4 || ctr10 == 9) begin
+          next_state = RYR;
+          next_ctr5 = 0;
+          next_ctr10 = 0;
+        end
+        else begin
+          // Stay in the same state, increment counters
+          next_state = RGR;
+          if (ctr5 != 0)
+            next_ctr5 = ctr5 + 1;
+          else if (!l_s)
+            next_ctr5 = 1;
+          if (ctr10 != 0)
+            next_ctr10 = ctr10 + 1;
+          else if (l_s && (s_s || n_s))
+            next_ctr10 = 1;
+        end
+      end
+    // Brute force way to make yellow last 2 cycles
+    RYR: next_state = RZR;
+    RZR: next_state = RHR;
+    // Check the sensors to determine the next state via priority
+    RHR: begin
+      if (n_s)
+        next_state = RRG;
+      else if (s_s)
+        next_state = GRR;
+      else if (l_s)
+        next_state = RGR;
+      else
+        next_state = RHR;
+      end
+    RRG: begin
+        // Check for 5 cycles of no traffic, or 10 cycles of traffic
+        if (ctr5 == 4 || ctr10 == 9) begin
+          next_state = RRY;
+          next_ctr5 = 0;
+          next_ctr10 = 0;
+        end
+        else begin
+          // Stay in the same state, increment counters
+          next_state = RRG;
+          if (ctr5 != 0)
+            next_ctr5 = ctr5 + 1;
+          else if (!n_s)
+            next_ctr5 = 1;
+          if (ctr10 != 0)
+            next_ctr10 = ctr10 + 1;
+          else if (n_s && (s_s || l_s))
+            next_ctr10 = 1;
+        end
+      end
+    // Brute force way to make yellow last 2 cycles
+    RRY: next_state = RRZ;
+    RRZ: next_state = RRH;
+    // Check the sensors to determine the next state via priority
+    RRH: begin
+      if (s_s)
+        next_state = GRR;
+      else if (l_s)
+        next_state = RGR;
+      else if (n_s)
+        next_state = RRG;
+      else
+        next_state = RRH;
+      end
     endcase
   end
 
 // combination output driver  ("C2" block in the Harris & Harris Moore machine diagram)
   always_comb begin
     str_light  = red;      // cover all red plus undefined cases
-	left_light = red;	   // default to red, then call out exceptions in case
-	ns_light   = red;
+    left_light = red;	   // default to red, then call out exceptions in case
+    ns_light   = red;
     case(present_state)    // Moore machine
       GRR:     str_light  = green;
-	  YRR,ZRR: str_light  = yellow;  // my dual yellow states -- brute force way to make yellow last 2 cycles
-	  RGR:     left_light = green;
-	  RYR,RZR: left_light = yellow;
-	  RRG:     ns_light   = green;
-	  RRY,RRZ: ns_light   = yellow;
+      YRR,ZRR: str_light  = yellow;  // my dual yellow states -- brute force way to make yellow last 2 cycles
+      RGR:     left_light = green;
+      RYR,RZR: left_light = yellow;
+      RRG:     ns_light   = green;
+      RRY,RRZ: ns_light   = yellow;
     endcase
   end
 
